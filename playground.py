cbfb8c1e6e0dd981cf3efb41816cfb9fbde65d7e
jeffchhen <jwchhen@ucdavis.edu>
2021-01-16 14:19:25 -0800

    added basic routing

--- playground.py
+++ playground.py
@@ -3,7 +3,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 @app.route('/')
 def yah():
-	print("yah");
 	return render_template('home.html')
 
 
@@ -16,10 +15,18 @@ def background_test():
 	print("yah");
 	return("nothing");
 
-@app.route("/connect4/<value>/")
-def connect4_home(value):
+@app.route("/connectN/")
+def connect4_home():
+
 	print("test Connect 4 page");
 	return render_template('connect4.html')
+@app.route("/battleship/")
+def battleship():
+	return render_template('battleship.html')
+@app.route("/hungergames/")
+def hungergames():
+	print("test Connect 4 page");
+	return render_template('hungergames.html')
 if __name__ == '__main__':
 	
 	app.run(debug=True)
